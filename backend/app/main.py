189689985988a4ea1ef07a788d2b46d0c509f758
rudraa19 <rudra19206@gmail.com>
2025-09-25 15:59:21 +0530

    feat: add modern swagger UI

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.routers import tasks
 from app.database.connection import init_db
+from scalar_fastapi import get_scalar_api_reference
 
 app = FastAPI()
 
@@ -13,3 +14,7 @@ app.include_router(tasks.router)
 @app.get("/")
 def root():
     return "Server is running"
+
+@app.get("/swagger", include_in_schema=False)
+async def scalar_html():
+    return get_scalar_api_reference(openapi_url=app.openapi_url, title=app.title)
